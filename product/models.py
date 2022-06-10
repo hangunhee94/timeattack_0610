a8a73dd3b6fafe24427c4fa963caee0fb9b64144
hangunhee94 <webzen9422@gmail.com>
2022-06-10 16:49:35 +0900

    model setting

--- product/models.py
+++ product/models.py
@@ -1,3 +1,52 @@
+from email.headerregistry import Address
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    class Meta:
+        db_table = "products"
+
+    name = models.CharField(max_length=256)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    image = models.CharField(max_length=256)
+    description = models.CharField(max_length=256)
+    price = models.CharField(max_length=256)
+    stock = models.CharField(max_length=256)
+    
+class Category(models.Model):
+    class Meta:
+        db_table = "categories"
+
+    name = models.CharField(max_length=256)
+
+class OrderStatus(models.Model):
+    class Meta:
+        db_table = "orders"
+
+    ordercomplete = models.CharField(max_length=256)
+    paymentcomplete = models.CharField(max_length=256)
+    cancel = models.CharField(max_length=256)
+    deliverystart = models.CharField(max_length=256)
+    deliverycomplete = models.CharField(max_length=256)
+
+class ProductOrder(models.Model):
+    class Meta:
+        db_table = "productorders"
+
+    productsave = models.CharField(max_length=256)
+
+class UserOrder(models.Model):
+    class Meta:
+        db_table = "userorders"
+    
+    address = models.CharField(max_length=256)
+    ordertime = models.CharField(max_length=256)
+    allprice = models.CharField(max_length=256)
+    sale = models.CharField(max_length=256)
+    finalprice = models.CharField(max_length=256)
+    validity = models.CharField(max_length=256)
+    
+    
+
+
+
--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    class Meta:
+        db_table = "users"
+
+    email = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+
